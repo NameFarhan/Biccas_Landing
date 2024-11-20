2894860d61ffc353777992a36c87e28b0c33ecdf
NameFarhan <Farhanalibwn1215@gmail.com>
2024-11-20 22:31:58 +0500

    Inital Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Box>
+      <Typography>Hey</Typography>
+    </Box>
+    </>
+  )
+}
+
+export default App
